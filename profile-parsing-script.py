46a6e210b9bddc9adead62485fa50fb37a1705dd
Haroon Choudery <haroon.choudery@gmail.com>
2017-10-31 22:21:05 -0400

    added new quality score file

---
+++ profile-parsing-script.py
@@ -0,0 +1,22 @@
+import os
+import json
+import csv
+
+PROFILES_DIRECTORY = 'profiles/'
+
+
+for file in os.listdir(PROFILES_DIRECTORY):
+	# open file (JSON)
+	# get number of followers
+	# for every post
+		# if isVideo == false and multipleImage == false:
+			# get urlImage
+			# get tags
+			# get description
+			# date (split into date and time)
+			# get numberLikes
+	
+	# save array into csv into file named
+
+
+
